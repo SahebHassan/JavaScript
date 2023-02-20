eb97a008566171cffdac3b4272efb287950da597
SahebHassan <124240923+SahebHassan@users.noreply.github.com>
2023-02-20 06:05:55 +0000

    practice function

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,29 +1,71 @@
-// // // // let weather=prompt('how is the weather')
+// // // // // let weather=prompt('how is the weather')
 
-// // // // if (weather=="rain"){
-// // // //   console.log('Grab your Umbrella')
-// // // // }
-// // // // else{
-// // // //   console.log('Wear the sunglasses')
-// // // // }
+// // // // // if (weather=="rain"){
+// // // // //   console.log('Grab your Umbrella')
+// // // // // }
+// // // // // else{
+// // // // //   console.log('Wear the sunglasses')
+// // // // // }
 
-// // const add=(a,b)=>{
-// //   return a+b
-// // }
-// // // console.log(add(12,18))
+// // // const add=(a,b)=>{
+// // //   return a+b
+// // // }
+// // // // console.log(add(12,18))
 
-// const add=(a,b)=>a+b
-// // console.log(add(12,15))
+// // const add=(a,b)=>a+b
+// // // console.log(add(12,15))
 
-const sub=(a,b)=>{
-  return a-b
+// const sub=(a,b)=>{
+//   return a-b
+// }
+// console.log(sub(12,5))
+
+// const div=(a,b)=>a/b
+// console.log(div(15,3))
+
+// const mul=(a,b)=>{
+//   return a*b
+// }
+// console.log(mul(15,15))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const add=(a,b)=>{
+  return a+b
 }
-console.log(sub(12,5))
+console.log(add(10,15))
+
+const sub=(a,b)=>a-b
+console.log(sub(10,5))
 
 const div=(a,b)=>a/b
-console.log(div(15,3))
+console.log(div(15,5))
 
-const mul=(a,b)=>{
+const mult=(a,b)=>{
   return a*b
 }
-console.log(mul(15,15))
+console.log(mult(10,10))
